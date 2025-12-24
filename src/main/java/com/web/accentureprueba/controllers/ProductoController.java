0aff8a0b839000860d2d4145449c1312993ac480
SebastianV91 <severgara91@hotmail.com>
2025-12-23 19:54:59 -0500

    CREACION MICROSERVICIO INSERTAR PRODUCTO

---
+++ src/main/java/com/web/accentureprueba/controllers/ProductoController.java
@@ -0,0 +1,29 @@
+package com.web.accentureprueba.controllers;
+
+import com.web.accentureprueba.dto.ProductoDTO;
+import com.web.accentureprueba.model.Producto;
+import com.web.accentureprueba.service.ProductoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/sucursales/{sucursalId}/productos")
+public class ProductoController {
+
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @PostMapping
+    public Mono<ResponseEntity<Producto>> crear(@PathVariable Long sucursalId,
+                                                @RequestBody ProductoDTO productoDTO){
+
+        return productoService.crear(sucursalId, productoDTO)
+                .map(producto -> ResponseEntity.status(HttpStatus.CREATED).body(producto));
+    }
+
+}
---
+++ src/main/java/com/web/accentureprueba/service/ProductoService.java
@@ -0,0 +1,36 @@
+package com.web.accentureprueba.service;
+
+import com.web.accentureprueba.dto.ProductoDTO;
+import com.web.accentureprueba.exception.NotFoundException;
+import com.web.accentureprueba.model.Producto;
+import com.web.accentureprueba.repository.ProductoRepository;
+import com.web.accentureprueba.repository.SucursalRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductoService {
+
+    private final ProductoRepository productoRepository;
+    private final SucursalRepository sucursalRepository;
+
+    public ProductoService(ProductoRepository productoRepository, SucursalRepository sucursalRepository) {
+        this.productoRepository = productoRepository;
+        this.sucursalRepository = sucursalRepository;
+    }
+
+    public Mono<Producto> crear(Long sucursalId, ProductoDTO productoDTO){
+
+        return sucursalRepository.findById(sucursalId)
+                .switchIfEmpty(Mono.error(new NotFoundException("Sucursal no encontrada")))
+                .flatMap(sucursal -> {
+                    Producto producto = new Producto();
+                    producto.setNombre(productoDTO.getNombre());
+                    producto.setStock(productoDTO.getStock());
+                    producto.setSucursalId(sucursalId);
+                    return productoRepository.save(producto);
+                });
+
+    }
+
+}
